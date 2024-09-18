4b484da387ebfca1beb79b83e9cca470d52d02f8
fraagu10 <aguilarfranco1092@gmail.com>
2024-09-18 12:08:03 -0500

    Created basic view for homepage of website, including bootstrap to customize look.

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
+    path('accounts/', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
