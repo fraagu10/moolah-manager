7e7cc103d0ed030754ed8b1c53b8dce99e8cd84c
fraagu10 <aguilarfranco1092@gmail.com>
2024-09-19 08:32:54 -0500

    Implemented ability for users to create a personal account.

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.sign_in, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
-    path('register/', views.register, name='register'),
+    path('register/', views.sign_up, name='register'),
     path('profile/', views.profile_detail_view, name='user_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -7,38 +7,47 @@ from .forms import LoginForm, RegisterForm
 from .models import User
 
 
-def sign_in(request):
-    if request.method == "GET":
-        if request.user.is_authenticated:
-            return redirect('user_profile')
-        form = LoginForm()
-        return render(request, 'users/login.html', {'form': form})
+# def sign_in(request):
+#     if request.method == "GET":
+#         if request.user.is_authenticated:
+#             return redirect('user_profile')
+#         form = LoginForm()
+#         return render(request, 'users/login.html', {'form': form})
+#
+#     elif request.method == "POST":
+#         form = LoginForm(request.POST)
+#
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = authenticate(request, username=username, password=password)
+#
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('user_profile')
+#             else:
+#                 messages.error(request, "Invalid username or password.")
+#                 return render(request, 'users/login.html', {'form': form})
 
-    elif request.method == "POST":
-        form = LoginForm(request.POST)
 
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('user_profile')
-            else:
-                messages.error(request, "Invalid username or password.")
-                return render(request, 'users/login.html', {'form': form})
-
-
-def register(request):
-    if request.method == "GET":
+def sign_up(request):
+    if request.method == 'GET':
         form = RegisterForm()
         return render(request, 'users/register.html', {'form': form})
 
-    if request == "POST":
-        pass
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('user_profile')
+        else:
+            return render(request, 'users/register.html', {"form": form})
+
 
 @login_required
 def profile_detail_view(request):
-    user_profile = get_object_or_404(User)
-    return render(request, 'users/profile.html', {'user_profile': user_profile})
+    user_query = User.objects.filter(username=request.user.username)
+    return render(request, 'users/profile.html', {'user_profile': user_query})
